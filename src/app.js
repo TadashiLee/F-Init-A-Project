deb28aa86e8034da9befebecdbcc5680dfe6f8d9
TadashiLee <584638605@qq.com>
2020-11-26 21:16:12 +0800

    [wenchang.li]feat:npm babel for react

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
-const sayNewHello = () => ("New Hello");
+import '@babel/polyfill';
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
 
-document.writeln(sayNewHello());
+class App extends Component {
+  render() {
+    return <div>22222</div>;
+  }
+}
+
+ReactDom.render(<App />, document.getElementById('root'));
